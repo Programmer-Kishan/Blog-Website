075942ba2f4ad62a1671dec9835dc40d91439c34
unknown <kishankathare45@gmail.com>
2023-06-15 21:08:26 +0530

    made navbar responsive

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './Navbar.module.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
 const Navbar = () => {
+
+    const [showMenu, setShowMenu] = useState(false);
+
+    const handleClick = () => {
+        console.log("Hello")
+        setShowMenu(!showMenu);
+    }
+
   return (
-    <nav className={classes.navbar}>
-        <h2>UpdateKnowledge</h2>
-        <ul>
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Blogs</a></li>
-            <li><a href='#'>News</a></li>
-            <li><a href='#'>Events</a></li>
-            <li><a href='#'>Jobs</a></li>
-        </ul>
-    </nav>
+    <>
+        <nav className={classes.navbar}>
+            <h2>UpdateKnowledge</h2>
+            <ul className={showMenu ? `${classes.menu} ${classes['mobile-links']}` : classes.menu}>
+                <li><a href='#'>Home</a></li>
+                <li><a href='#'>Blogs</a></li>
+                <li><a href='#'>News</a></li>
+                <li><a href='#'>Events</a></li>
+                <li><a href='#'>Jobs</a></li>
+            </ul>
+            <div className={classes["mobile-menu"]} onClick={handleClick}>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
+        </nav>
+    </>
   )
 }
 
